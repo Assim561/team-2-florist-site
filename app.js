0cfd6275a7f363b3a08c9f030a358c2cb3645621
jordanleeprogram <jordanleedignan@gmail.com>
2023-02-21 11:49:05 +0000

    arranged code to avoid merges, tested pages, ready to go

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ function test() {
 
 }
 
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("navbar").style.padding = "20px 10px";
+  } else {
+    document.getElementById("navbar").style.padding = "10px 10px";
+     }
+}
